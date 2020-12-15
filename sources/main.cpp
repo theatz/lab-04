a90869be4f0ff6740a8af0a8f2521fd23fb084cd
theatz <Ljvbybrfyf36>
2020-12-15 16:20:30 +0300

    test2

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,33 +6,15 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-  std::string pathToLookupDirectory = ".";
+  std::string path = ".";
   if (argc == 0) {
     //pathToLookupDirectory = argv[1];
-    std::cout << pathToLookupDirectory << std::endl;
+    std::cout << path << std::endl;
   }
 
   if (argc == 1) {
-    pathToLookupDirectory = argv[1];
-    std::cout << pathToLookupDirectory << std::endl;
+    path = argv[1];
+    std::cout << path << std::endl;
   }
-  /*
-  if (argc == 1) {
-    pathToLookupDirectory = ".";
-  } else if (argc == 2) {
-    pathToLookupDirectory = argv[1];
-  } else {
-    std::cout << "Error! Incorrect argument count" << std::endl;
-    std::cout << "Usage:" << std::endl;
-    std::cout << "lab-04-filesystem <path to directory>" << std::endl;
-    std::cout << "OR" << std::endl;
-    std::cout << "lab-04-filesystem" << std::endl;
-    std::cout << "which is equivalent to:" << std::endl;
-    std::cout << "lab-04-filesystem ." << std::endl;
-    return 1;
-  }
-  //DirectoryAnalyzer analyzer(pathToLookupDirectory);
-  //std::cout << analyzer;
-  return 0;
-   */
+
 }
